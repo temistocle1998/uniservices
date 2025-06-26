e2099f1923bed9f7d229a251466055315777b0a0
Mamadou lamine Bèye <mamadou.beye@isep-thies.edu.sn>
2025-06-26 16:32:52 +0000

    first commit

--- backend/user-service/src/test/java/com/uniservices/user_service/UserServiceApplicationTests.java
+++ backend/user-service/src/test/java/com/uniservices/user_service/UserServiceApplicationTests.java
@@ -2,6 +2,7 @@ package com.uniservices.user_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
